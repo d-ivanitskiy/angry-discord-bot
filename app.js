59801d2ddb7510e7eae305697a5f2db64b88e2db
Denis Iwanicki <ivanickij.da@reliab.tech>
2023-08-11 19:47:27 +0300

    empty bot initiate

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const { GatewayIntentBits } = require("discord.js");
+
+const client = new Discord.Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+    ]
+})
+
+client.login(process.env.BOT_TOKEN || '')
+    .then(() => console.log("bot is alive"))
+    .catch(console.error);

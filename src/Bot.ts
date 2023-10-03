335459b118f10f31012b990cfa2d877646605d8a
Denis Iwanicki <ivanickij.da@reliab.tech>
2023-10-03 19:12:49 +0300

    add extra intents & partials

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,17 +1,40 @@
 import dotenv from "dotenv";
-import Discord, { GatewayIntentBits, Partials } from "discord.js";
+import Discord, {GatewayIntentBits, Partials} from "discord.js";
 
 dotenv.config();
 
 const bot = new Discord.Client({
   intents: [
-    GatewayIntentBits.DirectMessages,
     GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildModeration,
     GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.AutoModerationConfiguration,
+    GatewayIntentBits.AutoModerationExecution,
   ],
-  partials: [Partials.Channel],
+  partials: [
+    Partials.User,
+    Partials.GuildScheduledEvent,
+    Partials.GuildMember,
+    Partials.ThreadMember,
+    Partials.Channel,
+    Partials.Message,
+    Partials.Reaction
+  ],
 });
 
 bot.on("messageCreate", (message) => {
@@ -21,6 +44,10 @@ bot.on("messageCreate", (message) => {
     console.log(message.content);
   }
 });
+
+bot.on('typingStart', (typing) => {
+  console.log(typing);
+})
 
 bot
   .login(process.env.BOT_TOKEN || "")
